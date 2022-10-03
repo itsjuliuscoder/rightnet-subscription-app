5905abefedebfc302d955ce04ac64178347756d4
Julius Olamide <juliusolajumokecoder@gmail.com>
2022-10-03 16:37:24 +0100

    tried fixing issues with database

--- utils/db.js
+++ utils/db.js
@@ -4,12 +4,12 @@ require('dotenv/config');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: process.env.DB_DIALECT,
     host: process.env.DB_HOST,
-    pool: {
-        max: 50,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+    // pool: {
+    //     max: 50,
+    //     min: 0,
+    //     acquire: 30000,
+    //     idle: 10000
+    // }
 });
 
 module.exports = sequelize;
